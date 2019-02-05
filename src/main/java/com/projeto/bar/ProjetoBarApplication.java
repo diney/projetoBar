f57c6ac6981e4c884c1e6645c8c0755659699def
Diney <diney.dom@gmail.com>
2019-02-05 14:14:45 -0200

    Projeto criado

---
+++ src/main/java/com/projeto/bar/ProjetoBarApplication.java
@@ -0,0 +1,14 @@
+package com.projeto.bar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoBarApplication.class, args);
+	}
+
+}
+
