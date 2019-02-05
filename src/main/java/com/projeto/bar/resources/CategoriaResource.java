342ce49f423b687b6e0e6f910c5f8fef1ee4a79d
Diney <diney.dom@gmail.com>
2019-02-05 19:04:02 -0200

    Criado repository e service para categoria

--- src/main/java/com/projeto/bar/resources/CategoriaResource.java
+++ src/main/java/com/projeto/bar/resources/CategoriaResource.java
@@ -1,16 +1,26 @@
 package com.projeto.bar.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.projeto.bar.domain.Categoria;
+import com.projeto.bar.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return"Rest está funfun";
+	@Autowired
+	private CategoriaService service;
+	
+	@RequestMapping(value ="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {		
+		Categoria obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
